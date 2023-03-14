dfac29ab70ba5ae7e42fc78354ffb7df24c1ee67
Jeral Pineda <jpineda@banhcafe.hn>
2023-03-14 14:14:40 -0600

    Creando segundo router y rutas

--- src/heroes/index.js
+++ src/heroes/index.js
@@ -1,2 +1,5 @@
 export * from './pages/DcPage';
 export * from './pages/MarvelPage';
+export * from './pages/Hero';
+export * from './pages/Search';
+export * from './routes/HeroesRoutes';
---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,22 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navbar } from '../../ui';
+import { DcPage, MarvelPage, Search, Hero } from '../index';
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container">
+        <Routes>
+          <Route path="marvel" element={<MarvelPage />} />
+          <Route path="dc" element={<DcPage />} />
+          <Route path="search" element={<Search />} />
+          <Route path="hero" element={<Hero />} />
+          {/*Search, Hero By Id  */}
+          <Route path="/" element={<Navigate to="marvel" />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
