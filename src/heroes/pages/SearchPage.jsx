7ae90ca285daa5119c3ee06bf395f353496958a6
Jeral Pineda <jpineda@banhcafe.hn>
2023-03-16 12:24:38 -0600

    Diseño de la pantalla de busqueda

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,3 +1,42 @@
+import { HeroCard } from '../components';
+
 export const SearchPage = () => {
-  return <div>Search</div>;
+  return (
+    <>
+      <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form>
+            <input
+              //
+              type="text"
+              name="searchText"
+              placeholder="Search a hero"
+              className="form-control"
+              autoComplete="off"
+            />
+            <button type="submit" className="btn btn-outline-primary mt-1 w-100">
+              Search
+            </button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-primary">Search a hero</div>
+
+          <div className="alert alert-danger">
+            No hero with <b>ABC</b>
+          </div>
+
+          <HeroCard />
+        </div>
+      </div>
+    </>
+  );
 };
